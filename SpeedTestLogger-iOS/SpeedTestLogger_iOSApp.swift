cb7af01ad6b983d5ff402c1aaf4fad4a2cc7c1b7
NakaokaRei <reideeplearning@gmail.com>
2022-12-01 20:54:28 +0900

    init

---
+++ SpeedTestLogger-iOS/SpeedTestLogger_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeedTestLogger_iOSApp.swift
+//  SpeedTestLogger-iOS
+//
+//  Created by rei.nakaoka on 2022/12/01.
+//
+
+import SwiftUI
+
+@main
+struct SpeedTestLogger_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
